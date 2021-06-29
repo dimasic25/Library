b7f91af97c2d5bc24f0944329b07bbd000cbc542
dimasic25 <dimonbubyr@mail.ru>
2021-06-29 16:07:46 +0300

    Initial commit

---
+++ src/main/java/org/springframework/boot/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+}
